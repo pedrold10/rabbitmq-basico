b3e8aec61007bc40e1c943597416e74878d05ba4
pedrold10 <pedrogabriel12423@gmail.com>
2023-09-28 14:27:48 -0300

    Prática - Roteamento de filas - RabbitMQ

---
+++ src/main/java/EmitLogDirect.java
@@ -0,0 +1,25 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class EmitLogDirect {
+
+  private static final String EXCHANGE_NAME = "direct_logs";
+
+  public static void main(String[] argv) throws Exception {
+    ConnectionFactory factory = new ConnectionFactory();
+    factory.setHost("localhost");
+    try (Connection connection = factory.newConnection();
+         Channel channel = connection.createChannel()) {
+        channel.exchangeDeclare(EXCHANGE_NAME, "direct");
+
+        String severity = "Olá";
+        String message ="Pedro Gabriel de Lima Diniz";
+
+        channel.basicPublish(EXCHANGE_NAME, severity, null, message.getBytes("UTF-8"));
+        System.out.println(" [x] Sent '" + severity + "':'" + message + "'");
+    }
+  }
+
+  
+}
